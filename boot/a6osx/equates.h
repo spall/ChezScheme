5fc0ed342519b3bfd56e4b10f47a5f7b597f44a0
Matthew Flatt <mflatt@racket-lang.org>
2017-11-17 14:32:48 -0700

    update bootfiles

--- boot/a6osx/equates.h
+++ boot/a6osx/equates.h
@@ -242,6 +242,8 @@ typedef unsigned long U64;
 #define fxvector_type_disp 0x1
 #define guardian_entry_next_disp 0x18
 #define guardian_entry_obj_disp 0x0
+#define guardian_entry_orderedp_disp 0x20
+#define guardian_entry_pending_disp 0x28
 #define guardian_entry_rep_disp 0x8
 #define guardian_entry_tconc_disp 0x10
 #define hashtable_default_size 0x8
@@ -262,7 +264,7 @@ typedef unsigned long U64;
 #define int_bits 0x20
 #define integer_divide_instruction 1
 #define keyboard_interrupt_index 0x3
-#define library_entry_vector_size 0x20C
+#define library_entry_vector_size 0x210
 #define libspec_closure_index 0xD
 #define libspec_does_not_expect_headroom_index 0x0
 #define libspec_error_index 0xE
@@ -489,7 +491,7 @@ typedef unsigned long U64;
 #define size_exactnum 0x20
 #define size_flonum 0x10
 #define size_forward 0x10
-#define size_guardian_entry 0x20
+#define size_guardian_entry 0x30
 #define size_inexactnum 0x20
 #define size_pair 0x10
 #define size_port 0x50
@@ -862,10 +864,14 @@ typedef unsigned long U64;
 #define GUARDIANREP(x) (*((ptr *)((uptr)(x)+8)))
 #define GUARDIANTCONC(x) (*((ptr *)((uptr)(x)+16)))
 #define GUARDIANNEXT(x) (*((ptr *)((uptr)(x)+24)))
+#define GUARDIANORDERED(x) (*((ptr *)((uptr)(x)+32)))
+#define GUARDIANPENDING(x) (*((ptr *)((uptr)(x)+40)))
 #define INITGUARDIANOBJ(x) (*((ptr *)((uptr)(x)+0)))
 #define INITGUARDIANREP(x) (*((ptr *)((uptr)(x)+8)))
 #define INITGUARDIANTCONC(x) (*((ptr *)((uptr)(x)+16)))
 #define INITGUARDIANNEXT(x) (*((ptr *)((uptr)(x)+24)))
+#define INITGUARDIANORDERED(x) (*((ptr *)((uptr)(x)+32)))
+#define INITGUARDIANPENDING(x) (*((ptr *)((uptr)(x)+40)))
 #define FORWARDMARKER(x) (*((ptr *)((uptr)(x)+0)))
 #define FORWARDADDRESS(x) (*((ptr *)((uptr)(x)+8)))
 #define CACHEDSTACKSIZE(x) (*((iptr *)((uptr)(x)+0)))
@@ -951,5 +957,5 @@ typedef unsigned long U64;
 #define VIRTREG(x,i) (((ptr *)((uptr)(x)+152))[i])
 
 /* library entries we access from C code */
-#define library_nonprocedure_code 150
-#define library_dounderflow 152
+#define library_nonprocedure_code 152
+#define library_dounderflow 154
