amirouche/arew

add make-arity-wrapper-procedure
A program can use `make-arity-wrapper-procedure` to synthesize a
function that reports a given arity mask (without calling `compile`).

In addition, `set-arity-wrapper-procedure!` suports modifying the
implementation of a synthesized procedure. Although similar
functionality could be achieved with `(lambda args (apply (unbox proc)
args))`, an arity wrapper procedure can dispatch to another procedure
without allocating a list for the arguments.

The interpreter now uses an internal variant of arity wrappers to
cooperate with `procedure-arity-mask`.

s/cmacros.ss
@@ -747,8 +747,10 @@
 (define-constant type-rtd-counts       #b01101110)
 (define-constant type-record                #b111)
 
-(define-constant code-flag-system         #b0001)
-(define-constant code-flag-continuation   #b0010)
+(define-constant code-flag-system           #b0001)
+(define-constant code-flag-continuation     #b0010)
+(define-constant code-flag-mutable-closure  #b0100)
+(define-constant code-flag-arity-in-closure #b1000)
 
 (define-constant fixnum-bits
   (case (constant ptr-bits)
@@ -835,6 +837,14 @@
   (fxlogor (constant type-code)
            (fxsll (constant code-flag-continuation)
                   (constant code-flags-offset))))
+(define-constant type-code-mutable-closure
+  (fxlogor (constant type-code)
+           (fxsll (constant code-flag-mutable-closure)
+                  (constant code-flags-offset))))
+(define-constant type-code-arity-in-closure
+  (fxlogor (constant type-code)
+           (fxsll (constant code-flag-arity-in-closure)
+                  (constant code-flags-offset))))
 
 ;; type checks are generally performed by applying the mask to the object
 ;; then comparing against the type code.  a mask equal to
@@ -909,6 +919,12 @@
            (fx- (fxsll 1 (constant code-flags-offset)) 1)))
 (define-constant mask-continuation-code
   (fxlogor (fxsll (constant code-flag-continuation) (constant code-flags-offset))
+           (fx- (fxsll 1 (constant code-flags-offset)) 1)))
+(define-constant mask-code-mutable-closure
+  (fxlogor (fxsll (constant code-flag-mutable-closure) (constant code-flags-offset))
+           (fx- (fxsll 1 (constant code-flags-offset)) 1)))
+(define-constant mask-code-arity-in-closure
+  (fxlogor (fxsll (constant code-flag-arity-in-closure) (constant code-flags-offset))
            (fx- (fxsll 1 (constant code-flags-offset)) 1)))
 (define-constant mask-thread       (constant byte-constant-mask))
 (define-constant mask-tlc          (constant byte-constant-mask))
@@ -2591,6 +2607,8 @@
      (nuate #f 0 #f #t)
      (virtual-register #f 1 #t #t)
      (set-virtual-register! #f 1 #t #t)
+     ($arity-wrapper-apply #f 0 #f #f)
+     (arity-wrapper-apply #f 0 #f #f)
   ))
 
 (let ()

s/primdata.ss
@@ -1119,6 +1119,8 @@
   (append! [sig [() -> (null)] [(list ... ptr) -> (ptr)]] [flags cp02])
   (apropos [sig [(sub-ptr) (sub-ptr environment) -> (void)]] [flags true])
   (apropos-list [sig [(sub-ptr) (sub-ptr environment) -> (list)]] [flags alloc])
+  (arity-wrapper-procedure? [sig [(ptr) -> (boolean)]] [flags pure unrestricted mifoldable discard])
+  (arity-wrapper-procedure-data [sig [(ptr) -> (ptr)]] [flags discard])
   (ash [sig [(sint sint) -> (sint)]] [flags arith-op mifoldable discard cp03])
   (assertion-violationf [sig [(who string sub-ptr ...) -> (bottom)]] [flags abort-op]) ; 2nd arg is format string
   (asinh [sig [(number) -> (number)]] [flags arith-op mifoldable discard])
@@ -1411,6 +1413,7 @@
   (logxor [sig [(sint ...) -> (sint)]] [flags arith-op partial-folder])
   (magnitude-squared [sig [(number) -> (number)]] [flags arith-op mifoldable discard])
   (make-annotation [sig [(ptr source-object ptr) (ptr source-object ptr annotation-options) -> (annotation)]] [flags pure true mifoldable discard])
+  (make-arity-wrapper-procedure [sig [(procedure sint ptr) -> (procedure)]] [flags pure true mifoldable discard])
   (make-boot-file [sig [(pathname sub-list pathname ...) -> (void)]] [flags true])
   (make-boot-header [sig [(pathname pathname pathname ...) -> (void)]] [flags true])
   (make-compile-time-value [sig [(ptr) (ptr ptr) -> (ptr)]] [flags pure unrestricted alloc])
@@ -1551,6 +1554,8 @@
   (s8-list->bytevector [sig [(sub-list) -> (bytevector)]] [flags alloc])
   (sc-expand [sig [(ptr) (ptr environment) (ptr environment ptr) (ptr environment ptr ptr) (ptr environment ptr ptr maybe-string) -> (ptr)]] [flags])
   (scheme-environment [sig [() -> (environment)]] [flags unrestricted alloc])
+  (set-arity-wrapper-procedure! [sig [(ptr procedure) -> (void)]] [flags true])
+  (set-arity-wrapper-procedure-data! [sig [(ptr ptr) -> (void)]] [flags true])
   (set-binary-port-input-buffer! [sig [(binary-input-port bytevector) -> (void)]] [flags true])
   (set-binary-port-input-index! [sig [(binary-input-port sub-index) -> (void)]] [flags true])
   (set-binary-port-input-size! [sig [(binary-input-port sub-length) -> (void)]] [flags true])
@@ -1732,6 +1737,7 @@
   ($closure-code [flags])
   ($closure-length [flags])
   ($closure-ref [flags])
+  ($closure-set! [flags])
   ($c-make-closure [flags])
   ($c-make-code [flags])
   ($code? [flags])
@@ -1740,6 +1746,8 @@
   ($code-arity-mask [flags])
   ($code-name [flags])
   ($code-pinfo* [flags])
+  ($code-mutable-closure? [flags])
+  ($code-arity-in-closure? [flags])
   ($collect-rendezvous [flags])
   ($compile-backend [flags])
   ($compiled-file-header? [flags])
@@ -2073,6 +2081,7 @@
   ($make-textual-input-port #;[sig [(string port-handler string) (string port-handler string ptr) -> (textual-input-port)]] [flags alloc])
   ($make-textual-output-port #;[sig [(string port-handler string) (string port-handler string ptr) -> (textual-output-port)]] [flags alloc])
   ($make-tlc [flags alloc])
+  ($make-arity-wrapper-procedure [flags])
   ($make-vtable [flags])
   ($map [flags])
   ($mark-invoked! [flags])

spall/ChezScheme

s/cmacros.ss
@@ -747,8 +747,10 @@
 (define-constant type-rtd-counts       #b01101110)
 (define-constant type-record                #b111)
 
-(define-constant code-flag-system         #b0001)
-(define-constant code-flag-continuation   #b0010)
+(define-constant code-flag-system           #b0001)
+(define-constant code-flag-continuation     #b0010)
+(define-constant code-flag-arity-in-closure #b0100)
+(define-constant code-flag-wrapper-closure  #b1000)
 
 (define-constant fixnum-bits
   (case (constant ptr-bits)
@@ -835,6 +837,14 @@
   (fxlogor (constant type-code)
            (fxsll (constant code-flag-continuation)
                   (constant code-flags-offset))))
+(define-constant type-code-arity-in-closure
+  (fxlogor (constant type-code)
+           (fxsll (constant code-flag-arity-in-closure)
+                  (constant code-flags-offset))))
+(define-constant type-code-wrapper-closure
+  (fxlogor (constant type-code)
+           (fxsll (constant code-flag-wrapper-closure)
+                  (constant code-flags-offset))))
 
 ;; type checks are generally performed by applying the mask to the object
 ;; then comparing against the type code.  a mask equal to
@@ -909,6 +919,12 @@
            (fx- (fxsll 1 (constant code-flags-offset)) 1)))
 (define-constant mask-continuation-code
   (fxlogor (fxsll (constant code-flag-continuation) (constant code-flags-offset))
+           (fx- (fxsll 1 (constant code-flags-offset)) 1)))
+(define-constant mask-code-arity-in-closure
+  (fxlogor (fxsll (constant code-flag-arity-in-closure) (constant code-flags-offset))
+           (fx- (fxsll 1 (constant code-flags-offset)) 1)))
+(define-constant mask-code-wrapper-closure
+  (fxlogor (fxsll (constant code-flag-wrapper-closure) (constant code-flags-offset))
            (fx- (fxsll 1 (constant code-flags-offset)) 1)))
 (define-constant mask-thread       (constant byte-constant-mask))
 (define-constant mask-tlc          (constant byte-constant-mask))
@@ -2591,6 +2607,8 @@
      (nuate #f 0 #f #t)
      (virtual-register #f 1 #t #t)
      (set-virtual-register! #f 1 #t #t)
+     ($arity-wrapper-apply #f 0 #f #f)
+     (arity-wrapper-apply #f 0 #f #f)
   ))
 
 (let ()

s/primdata.ss
@@ -1119,6 +1119,8 @@
   (append! [sig [() -> (null)] [(list ... ptr) -> (ptr)]] [flags cp02])
   (apropos [sig [(sub-ptr) (sub-ptr environment) -> (void)]] [flags true])
   (apropos-list [sig [(sub-ptr) (sub-ptr environment) -> (list)]] [flags alloc])
+  (arity-wrapper-procedure? [sig [(ptr) -> (boolean)]] [flags pure unrestricted mifoldable discard])
+  (arity-wrapper-procedure-data [sig [(ptr) -> (ptr)]] [flags discard])
   (ash [sig [(sint sint) -> (sint)]] [flags arith-op mifoldable discard cp03])
   (assertion-violationf [sig [(who string sub-ptr ...) -> (bottom)]] [flags abort-op]) ; 2nd arg is format string
   (asinh [sig [(number) -> (number)]] [flags arith-op mifoldable discard])
@@ -1411,6 +1413,7 @@
   (logxor [sig [(sint ...) -> (sint)]] [flags arith-op partial-folder])
   (magnitude-squared [sig [(number) -> (number)]] [flags arith-op mifoldable discard])
   (make-annotation [sig [(ptr source-object ptr) (ptr source-object ptr annotation-options) -> (annotation)]] [flags pure true mifoldable discard])
+  (make-arity-wrapper-procedure [sig [(procedure sint ptr) -> (procedure)]] [flags pure true mifoldable discard])
   (make-boot-file [sig [(pathname sub-list pathname ...) -> (void)]] [flags true])
   (make-boot-header [sig [(pathname pathname pathname ...) -> (void)]] [flags true])
   (make-compile-time-value [sig [(ptr) (ptr ptr) -> (ptr)]] [flags pure unrestricted alloc])
@@ -1551,6 +1554,8 @@
   (s8-list->bytevector [sig [(sub-list) -> (bytevector)]] [flags alloc])
   (sc-expand [sig [(ptr) (ptr environment) (ptr environment ptr) (ptr environment ptr ptr) (ptr environment ptr ptr maybe-string) -> (ptr)]] [flags])
   (scheme-environment [sig [() -> (environment)]] [flags unrestricted alloc])
+  (set-arity-wrapper-procedure! [sig [(ptr procedure) -> (void)]] [flags true])
+  (set-arity-wrapper-procedure-data! [sig [(ptr ptr) -> (void)]] [flags true])
   (set-binary-port-input-buffer! [sig [(binary-input-port bytevector) -> (void)]] [flags true])
   (set-binary-port-input-index! [sig [(binary-input-port sub-index) -> (void)]] [flags true])
   (set-binary-port-input-size! [sig [(binary-input-port sub-length) -> (void)]] [flags true])
@@ -1740,6 +1745,8 @@
   ($code-arity-mask [flags])
   ($code-name [flags])
   ($code-pinfo* [flags])
+  ($code-arity-in-closure? [flags])
+  ($code-wrapper-closure? [flags])
   ($collect-rendezvous [flags])
   ($compile-backend [flags])
   ($compiled-file-header? [flags])
@@ -2073,6 +2080,7 @@
   ($make-textual-input-port #;[sig [(string port-handler string) (string port-handler string ptr) -> (textual-input-port)]] [flags alloc])
   ($make-textual-output-port #;[sig [(string port-handler string) (string port-handler string ptr) -> (textual-output-port)]] [flags alloc])
   ($make-tlc [flags alloc])
+  ($make-arity-wrapper-procedure [flags])
   ($make-vtable [flags])
   ($map [flags])
   ($mark-invoked! [flags])